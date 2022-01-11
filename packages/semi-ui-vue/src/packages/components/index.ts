4df569f871d1c79588c28a06e06bb6eea5f410f9
kousum <1135192606@qq.com>
2022-01-11 10:31:11 +0800

    semi-theme-default

--- packages/semi-ui-vue/src/packages/components/index.ts
+++ packages/semi-ui-vue/src/packages/components/index.ts
@@ -1,1 +1,6 @@
+import "@douyinfe/semi-theme-default/scss/_font.scss";
+import "@douyinfe/semi-theme-default/scss/_palette.scss";
+import "@douyinfe/semi-theme-default/scss/global.scss";
+import "@douyinfe/semi-theme-default/scss/index.scss";
+
 export { default as Button } from './button'
