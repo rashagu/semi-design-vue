5a664d2d19b193e93cb2cba2911c3cd954961eb7
smw <1135192606@qq.com>
2022-10-12 17:09:36 +0800

    feat(gulp): 使用vite build

---
+++ packages/semi-ui-vue/build.js
@@ -0,0 +1,62 @@
+const {build} = require('vite')
+const fg = require('fast-glob')
+const vue = require("@vitejs/plugin-vue");
+const vueJsx = require("@vitejs/plugin-vue-jsx");
+const path = require("path");
+const gulp = require("gulp");
+
+
+
+gulp.task('buildSS', async function moveScss() {
+  // 获取默认为项目根目录下 src 文件夹包括子文件夹的所有 js、ts、jsx、tsx、vue、sevele 文件路径，除开 .test.*、.spec.* 和 .d.ts 三种后缀名的文件
+// 返回格式为 ['src/**/*.ts', 'src/**/*.tsx']
+  const srcFilePaths = fg.sync([`lib/**/*.js`], {
+    ignore: [`**/*.spec.*`, '**/*.test.*', '**/*.d.ts', '**/__tests__/**'],
+  });
+  return Promise.all(srcFilePaths.map(item=>{
+    const pathSS = path.resolve(__dirname, item)
+    const outDir = item.split('/')
+    const filename = outDir.splice(outDir.length-1, 1)
+    return build({
+      mode: 'production',
+      configFile: false,
+      logLevel: 'error',
+      build:{
+        outDir: outDir.join('/').replaceAll('lib','dist'),
+        lib: {
+          entry: pathSS,
+          formats: ['es'],
+          // the proper extensions will be added
+          fileName: filename[0].split('.')[0]
+        },
+        rollupOptions: {
+          // 确保外部化处理那些你不想打包进库的依赖
+          external: (source, importer, isResolved)=>{
+            return source.replaceAll('/').replaceAll('\\') !== pathSS.replaceAll('/').replaceAll('\\')
+          },
+          output: {
+            // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+            globals: {
+              vue: 'Vue'
+            }
+          }
+        },
+      },
+      plugins: [
+        vue(),
+
+        // @ts-ignore
+        vueJsx({
+          // options are passed on to @vue/babel-plugin-jsx
+        }),
+      ],
+    })
+  }))
+});
+
+const taskInstance = gulp.task('buildSS');
+if (taskInstance === undefined) {
+  console.error('no task named compileLib registered');
+  return;
+}
+taskInstance.apply(gulp);
--- packages/semi-ui-vue/vite.config.ts
+++ packages/semi-ui-vue/vite.config.ts
@@ -1,7 +1,52 @@
-import {defineConfig} from 'vite'
+import {defineConfig, build} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from "@vitejs/plugin-vue-jsx"
 import * as path from "path";
+
+build({
+  css:{
+
+  },
+  build:{
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      formats: ['es'],
+      name: 'SemiUiVue',
+      // the proper extensions will be added
+      fileName: 'semi-ui-vue'
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: [
+        "@douyinfe/semi-foundation",
+        "@douyinfe/semi-theme-default",
+        "@douyinfe/semi-animation",
+        "@kousum/semi-animation-vue",
+        "@kousum/semi-icons-vue",
+        "@kousum/semi-illustrations-vue",
+        "@kousum/vue3-window",
+        "async-validator",
+        "classnames",
+        "copy-text-to-clipboard",
+        "date-fns",
+        "date-fns-tz",
+        "lodash",
+        "@kousum/resize-observer-polyfill",
+        "sass",
+        "scroll-into-view-if-needed",
+        "utility-types",
+        "pinyin-pro",
+        "vue",
+      ],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    },
+  },
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,10 +64,25 @@ export default defineConfig({
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
       external: [
-        'vue',
-        '@douyinfe/semi-foundation',
-        '@douyinfe/semi-theme-default',
-        '@douyinfe/semi-animation'
+        "@douyinfe/semi-foundation",
+        "@douyinfe/semi-theme-default",
+        "@douyinfe/semi-animation",
+        "@kousum/semi-animation-vue",
+        "@kousum/semi-icons-vue",
+        "@kousum/semi-illustrations-vue",
+        "@kousum/vue3-window",
+        "async-validator",
+        "classnames",
+        "copy-text-to-clipboard",
+        "date-fns",
+        "date-fns-tz",
+        "lodash",
+        "@kousum/resize-observer-polyfill",
+        "sass",
+        "scroll-into-view-if-needed",
+        "utility-types",
+        "pinyin-pro",
+        "vue",
       ],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
