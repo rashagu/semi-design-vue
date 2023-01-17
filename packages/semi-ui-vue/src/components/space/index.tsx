96b677556cc61e269349d6deebb90587cdbc3b22
kousum <su1135192606@outlook.com>
2023-01-17 21:32:54 +0800

    fix: Space

--- packages/semi-ui-vue/src/components/space/index.tsx
+++ packages/semi-ui-vue/src/components/space/index.tsx
@@ -33,7 +33,7 @@ export const vuePropsType = {
     default: false
   },
   spacing: {
-    type: [String, Array],
+    type: [Number, Array],
     default: 'tight',
   },
   style: [String, Object],
@@ -42,52 +42,53 @@ export const vuePropsType = {
 const Index = defineComponent<SpaceProps>((props, {slots}) => {
 
 
-
-  const {
-    style,
-    className,
-    spacing,
-    wrap,
-    align,
-    vertical
-  } = props;
-  const isWrap = wrap && vertical ? false : wrap;
-  const realStyle = { ...style };
-  let spacingHorizontalType = '';
-  let spacingVerticalType = '';
-  if (isString(spacing)) {
-    spacingHorizontalType = spacing;
-    spacingVerticalType = spacing;
-  } else if (isNumber(spacing)) {
-    realStyle.rowGap = spacing;
-    realStyle.columnGap = spacing;
-  } else if (isArray(spacing)) {
-    if (isString(spacing[0])) {
-      spacingHorizontalType = spacing[0];
-    } else if (isNumber(spacing[0])) {
-      realStyle.columnGap = `${spacing[0]}px`;
-    }
-    if (isString(spacing[1])) {
-      spacingVerticalType = spacing[1];
-    } else if (isNumber(spacing[1])) {
-      realStyle.rowGap = `${spacing[1]}px`;
-    }
-  }
-  const classNames = cls(prefixCls, className, {
-    [`${prefixCls}-align-${align}`]: align,
-    [`${prefixCls}-vertical`]: vertical,
-    [`${prefixCls}-horizontal`]: !vertical,
-    [`${prefixCls}-wrap`]: isWrap,
-    [`${prefixCls}-tight-horizontal`]: spacingHorizontalType === strings.SPACING_TIGHT,
-    [`${prefixCls}-tight-vertical`]: spacingVerticalType === strings.SPACING_TIGHT,
-    [`${prefixCls}-medium-horizontal`]: spacingHorizontalType === strings.SPACING_MEDIUM,
-    [`${prefixCls}-medium-vertical`]: spacingVerticalType === strings.SPACING_MEDIUM,
-    [`${prefixCls}-loose-horizontal`]: spacingHorizontalType === strings.SPACING_LOOSE,
-    [`${prefixCls}-loose-vertical`]: spacingVerticalType === strings.SPACING_LOOSE,
-  });
   return ()=>{
+
+
+    const {
+      style,
+      className,
+      spacing,
+      wrap,
+      align,
+      vertical
+    } = props;
+    const isWrap = wrap && vertical ? false : wrap;
+    const realStyle:CSSProperties = { ...style };
+    let spacingHorizontalType = '';
+    let spacingVerticalType = '';
+    if (isString(spacing)) {
+      spacingHorizontalType = spacing;
+      spacingVerticalType = spacing;
+    } else if (isNumber(spacing)) {
+      realStyle.rowGap = spacing + 'px';
+      realStyle.columnGap = spacing + 'px';
+    } else if (isArray(spacing)) {
+      if (isString(spacing[0])) {
+        spacingHorizontalType = spacing[0];
+      } else if (isNumber(spacing[0])) {
+        realStyle.columnGap = `${spacing[0]}px`;
+      }
+      if (isString(spacing[1])) {
+        spacingVerticalType = spacing[1];
+      } else if (isNumber(spacing[1])) {
+        realStyle.rowGap = `${spacing[1]}px`;
+      }
+    }
+    const classNames = cls(prefixCls, className, {
+      [`${prefixCls}-align-${align}`]: align,
+      [`${prefixCls}-vertical`]: vertical,
+      [`${prefixCls}-horizontal`]: !vertical,
+      [`${prefixCls}-wrap`]: isWrap,
+      [`${prefixCls}-tight-horizontal`]: spacingHorizontalType === strings.SPACING_TIGHT,
+      [`${prefixCls}-tight-vertical`]: spacingVerticalType === strings.SPACING_TIGHT,
+      [`${prefixCls}-medium-horizontal`]: spacingHorizontalType === strings.SPACING_MEDIUM,
+      [`${prefixCls}-medium-vertical`]: spacingVerticalType === strings.SPACING_MEDIUM,
+      [`${prefixCls}-loose-horizontal`]: spacingHorizontalType === strings.SPACING_LOOSE,
+      [`${prefixCls}-loose-vertical`]: spacingVerticalType === strings.SPACING_LOOSE,
+    });
     const childrenNodes = slots.default?flatten(slots.default()):null;
-    return <div class={classNames} style={realStyle}>
+    return <div class={classNames} style={realStyle} x-semi-prop="children">
       {childrenNodes}
     </div>
   };
