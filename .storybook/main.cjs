d9142f9c3b33e77d790e08bf922b562f9eba4f0c
smw <1135192606@qq.com>
2023-01-31 17:32:55 +0800

    storybook

---
+++ .storybook/main.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  "stories": [
+    "../packages/**/src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  }
+}
