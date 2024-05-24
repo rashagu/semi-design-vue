d342e8dee65c67006977813fddb0f89aacec9a74
kousum <kousumwork@outlook.com>
2024-05-24 15:37:10 +0800

    fix: descriptions pre

--- packages/semi-ui-vue/src/components/descriptions/index.tsx
+++ packages/semi-ui-vue/src/components/descriptions/index.tsx
@@ -92,40 +92,55 @@ const Descriptions = defineComponent<DescriptionsProps>(
     const adapter = adapter_();
     const foundation = new DescriptionsFoundation<DescriptionsProps>(adapter);
 
-    function getHorizontalList() {
-      const { column } = props;
-      const columns = foundation._adapter.getColumns();
-      const horizontalList = [];
-      const curSpan = { totalSpan: 0, itemList: [] };
-      for (const item of columns) {
-        curSpan.totalSpan += item.span || 1;
-        if (curSpan.totalSpan > column) {
-          if (curSpan.itemList.length < column) {
-            const lastSpan = curSpan.itemList[curSpan.itemList.length - 1];
-            if (isNaN(lastSpan.span)) {
-              lastSpan.span = column - curSpan.itemList.length + (lastSpan.span || 1);
-            }
-          }
-          horizontalList.push(curSpan.itemList);
-          curSpan.itemList = [];
-          curSpan.totalSpan = item.span || 1;
-        }
-        curSpan.itemList.push(item);
+    // 当一行里面的列数没有达到maxColumnPerLine，就让最后一列填满剩余的
+    function _rowFillRemainder(itemRow: any) {
+      const { column: maxColumnPerLine } = adapter.getProps();
+      if (itemRow.length === 0) {
+        return;
+      }
+      const lastSpan = itemRow[itemRow.length - 1];
+      // 如果用户设置了span就跳过
+      if (lastSpan.span && !isNaN(lastSpan.span)) {
+        return;
       }
 
-      if (curSpan.itemList.length != 0) {
-        const lastSpan = curSpan.itemList[curSpan.itemList.length - 1];
-        if (isNaN(lastSpan.span)) {
-          let total = 0;
-          curSpan.itemList.forEach(item=>{
-            return total += !isNaN(item.span)?item.span:1;
-          });
-          if (total < column) {
-            lastSpan.span = column - total + 1;
-          }
-        }
-        horizontalList.push(curSpan.itemList);
+      let total = 0;
+      itemRow.forEach((item: { span: number })=>{
+        return total += !isNaN(item.span)?item.span:1;
+      });
+
+      if (total < maxColumnPerLine) {
+        lastSpan.span = maxColumnPerLine - total + 1;
       }
+    }
+
+    function getHorizontalList() {
+      const { column: maxColumnPerLine } = adapter.getProps();
+      const columns = adapter.getColumns();
+      const horizontalList = [];
+      const curRow = { totalSpan: 0, itemList: [] };
+      columns.forEach((item, index) => {
+        let itemSpan = item.span || 1;
+        let restSpan = maxColumnPerLine - curRow.totalSpan;
+        if (itemSpan <= restSpan) {
+          curRow.itemList.push(item);
+          curRow.totalSpan = curRow.totalSpan + itemSpan;
+        } else {
+          // 剩余空间放不下当前item，需要另起一行
+          // 若新行放不下当前item，极端情况，例如用户给当前span数值远大于 maxColumnPerLine，即使另起新行都无法放得下，itemSpan直接按props.column处理
+          itemSpan > maxColumnPerLine ? itemSpan = maxColumnPerLine : null;
+          // 新行能放得下当前item，将原有行push到horizontalList中，然后重置curRow，将当前item存到curRow中
+          _rowFillRemainder(curRow.itemList);
+          horizontalList.push(curRow.itemList);
+          curRow.totalSpan = itemSpan;
+          curRow.itemList = [item];
+        }
+        if (index === columns.length -1) {
+          _rowFillRemainder(curRow.itemList);
+          horizontalList.push(curRow.itemList);
+        }
+      });
+
       return horizontalList;
     }
     const renderChildrenList = () => {
