d9833d10793ea5ec3f3cca6da3ada4c31db9ec59
kousum <kousumwork@outlook.com>
2024-07-04 15:10:42 +0800

    web-types: Doesn't seem very necessary

--- packages/semi-ui-vue/scripts/test.ts
+++ packages/semi-ui-vue/scripts/test.ts
@@ -1,41 +1,63 @@
-import { Project, SyntaxKind, TypeFormatFlags } from 'ts-morph';
-import { inspect } from './types';
-// import {Button} from "@kousum/semi-ui-vue/src/components/button/Button.tsx";
+import { Project, SyntaxKind, Type } from "ts-morph";
+import * as fs from "fs";
+import { componentsData } from './componentsData';
 
-// const tsMorph = require('ts-morph');
-// 初始化项目
+// 初始化 ts-morph 项目
 const project = new Project({
   tsConfigFilePath: "tsconfig.json",
-  // addFilesFromTsConfig: true,
 });
 
-// 加载文件
-const sourceFile = project.addSourceFileAtPath('dist/button/ButtonGroup.d.ts');
+// 解析 TSX 组件文件
+const sourceFiles = project.addSourceFilesAtPaths('./node_modules/@kousum/semi-ui-vue/dist/**/*.d.ts');
+const sourceFilesNames = project.addSourceFilesAtPaths('./node_modules/@kousum/semi-ui-vue/dist/index.d.ts');
 
-const props = [];
-const interfaces = sourceFile.getInterfaces();
+const webTypes = {
+  $schema: "https://raw.githubusercontent.com/JetBrains/web-types/master/schema/web-types.json",
+  framework: "vue",
+  name: "my-vue-library",
+  version: "1.0.0",
+  contributions: {
+    html: {
+      tags: [],
+    },
+  },
+};
 
-interfaces.forEach((i) => {
-  if (i.getName() === 'ButtonGroupProps') {  // 假设你的 props 接口名为 Props
-    i.getProperties().forEach((property) => {
-      props.push({
-        name: property.getName(),
-        type: property.getType().getText(),
-        required: !property.hasQuestionToken(),
-      });
-    });
-    i.getBaseDeclarations().forEach((property) => {
-      property.getType().getProperties().forEach((s) => {
-        const type = s.getValueDeclaration().getType().getText(undefined, TypeFormatFlags.None)
-        const required = !s.isOptional();
-        props.push({
-          name: s.getName(),
-          type,
-          required,
-        });
-      })
+
+
+sourceFiles.forEach((sourceFile) => {
+  sourceFile.getInterfaces().forEach(item=>{
+    let find = componentsData.find(component=>{
+      return component.propsName === item.getName()
     })
-  }
+    if (find){
+      const componentTag = {
+        name: find.name,
+        attributes: [],
+        events: [],
+        slots: [],
+      };
+      item.getType().getProperties().forEach(t=>{
+        let propType = t.getValueDeclaration()?.getType().getText();
+        if (propType?.indexOf('node_modules/') > -1){
+          propType = '(\"' + propType.split('node_modules/').pop()
+        }
+        const propName = t.getName();
+        componentTag.attributes.push({
+          name: propName,
+          description: `Description for ${propName}`,
+          value: {
+            type: propType,
+          },
+        });
+        // console.log(t.getName(), propType)
+      })
+      webTypes.contributions.html.tags.push(componentTag);
+    }
+  })
+
+
 });
 
-console.log(JSON.stringify(props, null, 2));
+// 将 web-types 写入文件
+fs.writeFileSync("web-types.json", JSON.stringify(webTypes, null, 2));
